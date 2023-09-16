44c6902e163b2484fd1f797a12f952b6ebf53135
ky-catlog <101172692+ky-catlog@users.noreply.github.com>
2023-09-17 00:17:12 +0100

    modified search bindings

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -6,10 +6,7 @@ M.plugins = 'custom.plugins'
 local opts = { noremap = true ,silent =true}
 
 vim.keymap.set("n", "ff", function()
-  local input_string = "" .. vim.fn.expand "<cword>"
-  if input_string == "" then
-    return
-  end
+  local input_string = "" .. vim.fn.expand "<cword>" 
   require("telescope.builtin").grep_string {
     search = input_string,
   }
@@ -23,7 +20,4 @@ vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts) 
 vim.keymap.set('n', ';', '<C-u>', opts) 
 vim.keymap.set('n', 'm', '<C-d>', opts) 
-vim.keymap.set("n", "<leader>rn", function()
-    return ":IncRename " .. vim.fn.expand "<cword>"
-  end, { expr = true }) 
 return M
