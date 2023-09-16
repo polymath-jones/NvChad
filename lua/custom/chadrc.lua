fb7982bf87d44d3dc086e693782b45a016620298
ky-catlog <101172692+ky-catlog@users.noreply.github.com>
2023-09-17 00:19:42 +0100

    minor fix

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -17,7 +17,9 @@ vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
-vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts) 
 vim.keymap.set('n', ';', '<C-u>', opts) 
 vim.keymap.set('n', 'm', '<C-d>', opts) 
+vim.keymap.set("n", "<leader>rn", function()
+    return ":IncRename " .. vim.fn.expand "<cword>"
+  end, { expr = true }) 
 return M
