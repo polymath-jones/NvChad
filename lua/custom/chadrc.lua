9e7e403cae6d44100201a8ee93962a2a2f1a2c1c
ky-catlog <101172692+ky-catlog@users.noreply.github.com>
2023-09-19 00:54:14 +0100

    added new keybindings

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,12 +1,15 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'penumbra_dark' }
+M.ui = { theme = 'onedark'  }
 M.plugins = 'custom.plugins'
 local opts = { noremap = true ,silent =true}
 
 vim.keymap.set("n", "ff", function()
-  local input_string = "" .. vim.fn.expand "<cword>" 
+  local input_string = "" .. vim.fn.expand "<cword>"
+  if input_string == "" then
+    return
+  end
   require("telescope.builtin").grep_string {
     search = input_string,
   }
@@ -14,6 +17,8 @@ end)
 vim.keymap.set("i", "jk", "<Esc>", opts)
 vim.keymap.set("n", "Q", "<Cmd>q<CR>", opts)
 vim.keymap.set("n", "qq", "<Cmd>cclose<CR>", opts)
+vim.keymap.set("n", "<C-l>", "<Cmd>SessionManager load_session<CR>", opts)
+vim.keymap.set("n", ",,", "<Cmd>noh<CR>", opts)
 vim.keymap.set('n', 't', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'T', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
